c6a1c631de0146e53ddf82b12138bcf7e773ccee
Anatoliy0540 <61547560+Anatoliy0540@users.noreply.github.com>
2021-01-02 09:30:19 -0500

    Initial commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML lang="en">
+<html>
+	<head>
+		<title>Anatoliy Barnes - Landscape Artist</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<section id="header">
+
+					<!-- Logo -->
+						<h1><a href="index.php">Dopetrope</a></h1>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="index.php">Home</a></li>
+							</ul>
+						</nav>
+					</section>
